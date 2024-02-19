8e24b40df6d6f4d2e11ae5009a29fb396e42b604
yaroslavss <yaroslav.syuzev@gmail.com>
2024-02-19 12:38:00 +0400

    Remove compose + add xml views

---
+++ app/src/main/java/com/yara/android_practicum/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yara.android_practicum.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yara.android_practicum.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
