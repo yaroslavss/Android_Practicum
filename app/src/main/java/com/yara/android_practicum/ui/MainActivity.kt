094473ab092accfeec440760b32f80bc55dd1b0f
yaroslavss <yaroslav.syuzev@gmail.com>
2024-02-21 21:45:29 +0400

    Add images for profile fragment + fix to CoordinatorLayout

--- app/src/main/java/com/yara/android_practicum/ui/MainActivity.kt
+++ app/src/main/java/com/yara/android_practicum/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.yara.android_practicum.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yara.android_practicum.R
+import androidx.appcompat.app.AppCompatActivity
+import com.yara.android_practicum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/yara/android_practicum/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yara/android_practicum/ui/profile/ProfileFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.yara.android_practicum.databinding.FragmentProfileBinding
 
@@ -20,6 +21,12 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.toolbar)
     }
 
     override fun onDestroyView() {
