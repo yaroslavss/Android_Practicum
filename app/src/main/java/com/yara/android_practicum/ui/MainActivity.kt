1d7db32f044c4a2ba8792b2db98b435cbf7386e1
yaroslavss <yaroslav.syuzev@gmail.com>
2024-02-24 18:25:38 +0400

    Adjust AppBarLayout and CollapsingToolbarLayout

--- app/src/main/java/com/yara/android_practicum/ui/MainActivity.kt
+++ app/src/main/java/com/yara/android_practicum/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.yara.android_practicum.ui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yara.android_practicum.R
@@ -11,6 +15,7 @@ import com.yara.android_practicum.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding;
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +23,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // set the toolbar as the app bar for the activity
+        val appBar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(appBar)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.profileFragment,
+            )
+        )
+
         // set up navigation
         val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
