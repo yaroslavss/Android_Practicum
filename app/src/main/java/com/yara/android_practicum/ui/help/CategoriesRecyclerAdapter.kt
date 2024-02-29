e438e16f360e9c5cbf36b794e61981b8909c8e4b
yaroslavss <yaroslav.syuzev@gmail.com>
2024-02-29 22:02:26 +0400

    Add categories RecyclerView draft to help fragment

---
+++ app/src/main/java/com/yara/android_practicum/ui/help/CategoriesRecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package com.yara.android_practicum.ui.help
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yara.android_practicum.databinding.ItemCategoryBinding
+import com.yara.android_practicum.domain.model.Category
+
+class CategoriesRecyclerAdapter(var categories: List<Category>) :
+    RecyclerView.Adapter<CategoriesRecyclerAdapter.CategoryViewHolder>() {
+
+    private var _binding: ItemCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        _binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val item = categories[position]
+        holder.itemView.apply {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+}
