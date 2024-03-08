6684647610473a7f066f028f93c1a8214ae4a5ad
yaroslavss <yaroslav.syuzev@gmail.com>
2024-03-08 19:54:21 +0400

    Refactor mapper

--- app/src/main/java/com/yara/android_practicum/ui/help/HelpViewModel.kt
+++ app/src/main/java/com/yara/android_practicum/ui/help/HelpViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.yara.android_practicum.data.Mappers
 import com.yara.android_practicum.data.datasource.HardCodedDataSource
+import com.yara.android_practicum.data.mapper.toDomainModelList
 import com.yara.android_practicum.data.repository.CategoriesRepositoryImpl
 import com.yara.android_practicum.domain.model.Category
 import kotlinx.coroutines.Dispatchers
@@ -25,7 +25,7 @@ class HelpViewModel : ViewModel() {
     private fun loadCategories() {
         viewModelScope.launch(Dispatchers.IO) {
             val categories = categoriesRepository.loadCategories()
-            _categoriesListLiveData.postValue(Mappers.CategoryToDomainListMapper.map(categories))
+            _categoriesListLiveData.postValue(categories.toDomainModelList())
         }
     }
 }
--- app/src/main/java/com/yara/android_practicum/utils/Mapper.kt
+++
@@ -1,6 +0,0 @@
-package com.yara.android_practicum.utils
-
-fun interface Mapper<in A, out B> {
-
-    fun map(type: A?): B
-}
