6f7de794ec5b585d04c416893675580bfcf27134
yaroslavss <yaroslav.syuzev@gmail.com>
2024-03-12 11:07:16 +0400

    Add empty news fragment

---
+++ app/src/main/java/com/yara/android_practicum/ui/news/NewsFragment.kt
@@ -0,0 +1,38 @@
+package com.yara.android_practicum.ui.news
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.yara.android_practicum.R
+import com.yara.android_practicum.databinding.FragmentNewsBinding
+
+class NewsFragment : Fragment() {
+
+    private var _binding: FragmentNewsBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<NewsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.title = getString(R.string.news_fragment_label)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/yara/android_practicum/ui/news/NewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.yara.android_practicum.ui.news
+
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
