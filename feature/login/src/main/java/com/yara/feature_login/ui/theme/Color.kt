26cdf7531ba34dec8bbe064663c162e433c24797
yaroslavss <yaroslav.syuzev@gmail.com>
2024-06-04 16:14:28 +0400

    Add more controls to login screen

--- feature/login/src/main/java/com/yara/feature_login/ui/theme/Color.kt
+++ feature/login/src/main/java/com/yara/feature_login/ui/theme/Color.kt
@@ -2,5 +2,8 @@ package com.yara.feature_login.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Leaf = Color(0xff66a636)
+
 val Black38 = Color(0x61000000)
 val Black70 = Color(0xb3000000)
+val White = Color(0xFFFFFFFF)
