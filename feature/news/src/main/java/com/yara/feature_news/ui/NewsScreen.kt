6181aa62839dc05abff9b8ced3789a1d377fb686
yaroslavss <yaroslav.syuzev@gmail.com>
2024-06-08 10:07:05 +0400

    Add composable progress bar to news screen

--- feature/news/src/main/java/com/yara/feature_news/ui/NewsScreen.kt
+++ feature/news/src/main/java/com/yara/feature_news/ui/NewsScreen.kt
@@ -1,10 +1,15 @@
 package com.yara.feature_news.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,7 +18,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +29,7 @@ import com.yara.feature_news.R
 import com.yara.feature_news.ui.theme.AndroidPracticumTheme
 import com.yara.feature_news.ui.theme.AppBarText
 import com.yara.feature_news.ui.theme.Leaf
+import com.yara.feature_news.ui.theme.TurtleGreen
 import com.yara.feature_news.ui.theme.White
 
 @Composable
@@ -45,9 +53,13 @@ fun NewsScreenContent(
         Column {
             CustomAppBar(buttonOnClick)
 
-            LazyColumn(Modifier.fillMaxSize()) {
-                items(uiState.events) {
-                    EventItem(it, itemOnClick)
+            if (uiState.isLoading) {
+                ProgressBar()
+            } else {
+                LazyColumn(Modifier.fillMaxSize()) {
+                    items(uiState.events) {
+                        EventItem(it, itemOnClick)
+                    }
                 }
             }
         }
@@ -78,6 +90,23 @@ fun CustomAppBar(buttonOnClick: () -> Unit) {
             }
         },
     )
+}
+
+@Composable
+fun ProgressBar() {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize(),
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(dimensionResource(R.dimen.progress_bar_width)),
+            color = TurtleGreen,
+            trackColor = White,
+        )
+    }
 }
 
 @Preview
